61ece2302ba952fecc71e6c7115b2d1a22b171d0
Elson Santos <elsonss1988@gmail.com>
2021-03-08 15:55:44 -0300

    test: gravar media no BD

--- controllers/AlunoController.js
+++ controllers/AlunoController.js
@@ -42,23 +42,52 @@ exports.atualizarAluno = async (req, h) => {
   
 }
 
-exports.calcularMedia = (req, h)  => {
+exports.calcularMedia = async (req, h) => {
+
+  /************************************** /
+   *  apresentacao comentando pois      *
+   * nao vem no payload do req          * 
+   * ********************************** */
+  
   const db = req.server.plugins['hapi-mongodb'].db;
   const repoAlunos = new AlunosRepository(db);
-  
-    const p1 = 30;
-		const p2 = 30;
-		const p3 = 15;
-		const p4 = 25;  
-
+    
+   //media
+   console.log('req:',req)
+   let conceito
+   let status
    let prova1=req.payload["prova1"]
    let prova2=req.payload['prova2']
    let trabalho=req.payload['trabalho']
-   let apresentacao=req.payload['apresentacao']
-   let media = ((prova1 * p1) + (prova2 * p2) + (trabalho * p3) + (apresentacao * p4)) / (p1 + p2 + p3 + p4)
-
-   console.log(req.payload);
-   console.log('Sua média ponderada é: ' + media); 
+   console.log('apresen',req.payload['apresentacao']);
+   //let apresen=req.apresentacao['apresentacao']
+   let media= (prova1+prova2+trabalho)/3 //+apresentacao)/4
+   console.log(media); 
   
+   if(media>5)
+   {status='aprovado'
+      if (media>9){
+        conceito='A'
+      }
+      if (media>7){
+        conceito='B'
+      }else conceito='C'
+    
+   }
+   if(media<5)
+   {
+    status='reprovado'
+     if (media<3){
+      conceito='E'    }
+    else conceito='F'
+  }
+  
+
+    
+    var myquery = { status: "Valley 345" };
+    var newvalues = { $set: {media : "Mickey", conceito: "Canyon 123",  } };
+    db.aluno("alunos").updateOne(myquery, newvalues)
+    
     return media
+
 }
