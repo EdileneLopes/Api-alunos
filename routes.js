1c1f86134d1be8bf71c3d2b4467fea629b9614eb
Rubenscode <rubens.andrade1@gmail.com>
2021-03-09 19:05:18 -0300

    Registrado swagger no server e adicionado tag para mapeamento das rotas

--- routes.js
+++ routes.js
@@ -4,31 +4,49 @@ module.exports = [
     {
       method: 'GET',
       path: '/alunos',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.listarAlunos
     },
     {
       method: 'GET',
       path: '/alunos/{id}',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.buscarAluno
     },
     {
       method: 'POST',
       path: '/alunos',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.inserirAluno
     },
     {
       method: 'DELETE',
       path: '/alunos/{id}',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.apagarAluno
     },
     {
       method: 'PUT',
       path: '/alunos/{id}',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.atualizarAluno
     },
     {
       method: 'PUT',
       path: '/alunos/{id}/media',
+      options: {
+        tags: ['api']
+      },
       handler: AlunoController.calcularMedia
     }
 ]
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const Hapi = require('@hapi/hapi');
+const swaggered = require('hapi-swaggered');
+const swaggeredUI = require('hapi-swaggered-ui');
+const vision = require('vision');
+const inert = require('inert');
 const rotas = require('./routes.js');
 const MongoDbMiddleware = require('./middlewares/MongoDbMiddleware.js');
 
@@ -9,13 +13,56 @@ const MongoDbMiddleware = require('./middlewares/MongoDbMiddleware.js');
     host: 'localhost'
   });
 
-  
+  await server.register([
+    inert,
+    vision,
+    {
+      plugin: swaggered,
+      options: {
+        info: {
+          title: 'Persistência Poliglota',
+          description: 'API - Sistema de Gerenciamento de Alunos - Que permite o Cadastro, Inclusão, Consulta e Exclusão de Alunos com Autenticação JSON Web Token - Persistência Poliglota',
+          version: '1.0.0',
+          termsOfService: "https://swagger.io/terms/",
+          contact: {
+            name: "Gamados por Java",
+            url: "https://github.com/EdileneLopes/Api-alunos",
+            email: "lopes_edi@yahoo.com.br"
+          },
+          license: {
+            name: "MIT - LICENSE",
+            url: "https://github.com/EdileneLopes/Api-alunos/blob/master/LICENSE"
+          }
+        }
+      }
+    },
+    {
+      plugin: swaggeredUI,
+      options: {
+        title: 'Example API',
+        path: '/docs',
+        authorization: {
+          field: 'apiKey',
+          scope: 'query', // header works as well
+          // valuePrefix: 'bearer '// prefix incase
+          defaultValue: 'demoKey',
+          placeholder: 'Enter your apiKey here'
+        },
+        swaggerOptions: {
+          validatorUrl: null
+        }
+      }
+    }
+  ])
+
   await MongoDbMiddleware(server);
 
   rotas.forEach(rota => server.route(rota));
 
   await server.start();
+  
   console.log('Nosso servidor de alunos está rodando em !', server.info.uri);
 
 })()
+
 
