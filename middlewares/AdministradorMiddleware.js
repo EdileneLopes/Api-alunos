09b466235682a56157cb4c2dea2930df3ffaa618
edilenelopes <lopes_edi@yahoo.com.br>
2021-03-10 11:07:02 -0300

    Função jwt ainda sem teste

---
+++ middlewares/AdministradorMiddleware.js
@@ -0,0 +1,25 @@
+const JwtPlugin = require('@hapi/jwt');
+
+module.exports = async (server) => {
+  await server.register(JwtPlugin);
+
+  server.auth.strategy('autorizacao_jwt', 'jwt', {
+    keys: 'chavesecreta',
+    verify: {
+        aud: false,
+        iss: false,
+        sub: false,
+        nbf: false,
+        exp: false,
+        maxAgeSec: 84600, // 24 hours
+        timeSkewSec: 15
+    },
+    validate: (artifacts, request, h) => {
+        return {
+            isValid: true,
+        };
+    }
+  });
+
+  server.auth.default('autorizacao_jwt');
+};
